bce4f0bfb94b6b7a29893bb5a5c7a6646e4975b3
TemeAvramova <nushy_avramova@hotmail.it>
2017-12-30 00:42:23 +0100

    Ho tolto la variabile 'direzione_paper' che imponeva l'alternarsi di PN e PG come era nel paper e ora il passaggio da PN a PG e' dovuto direttamente alla disuguaglianza (5)

--- m/esempio8_con_variabili_da_paper.m
+++ m/esempio8_con_variabili_da_paper.m
@@ -49,16 +49,20 @@ load parametri_paper
 load XX %sono le soluzioni del paper x del sistema nonlineare ad ogni iterazione
 load nu_kv %nu_k ricavate dal paper
 nu_k_definitive =  nu_k_vera+0.00001;
+%% aggiusto le dimensioni
+nu_k_definitive = [nu_k_definitive(1:4),0,nu_k_definitive(5:8), nu_k_definitive(8),nu_k_definitive(9:11),nu_k_definitive(11), nu_k_definitive(12:end)];
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 ss=1;
-
+FLAG=0;
 nu_k_vera(end+1)=nu_k_vera(end);
-while err>tol && k<22
+while err>tol && k<25
      k=k+1;
      
-     FLAG=direzione_paper(k);
+     %FLAG=direzione_paper(k);
+     
      if FLAG==0 
+     
          nu_k=nu_k_definitive(ss);
          [d, ~, ~, it, ~, duvec] = gmresx (J(x), -F(x),100 , nu_k, 100, [], [], zeros (100, 1));
          ss=ss+1;
@@ -88,20 +92,31 @@ while err>tol && k<22
           
      else
            d= -DOMEGA(F(x),J(x));
-           m=mm_paper(k+1);
-           lambdap = lambda0g^m;  
-           lambda(k)=lambdap;
-            x0=x;
-            x=P(x + lambdap*d); 
-            sen_2(k)=1;
-            mm(k)=m;
-                 
+            lambdap= 0.8; %qui non sono riuscita ad aggiungere il ciclo while con la disuguaglianza (6) perchè come si vede dai risultati stampati, 
+            % questa non è MAI verificata per lambdap=0.8.
+            lambdac=0.8;
+            printf("iterazione = %d\n", k)
+            printf ("lhs della disuguaglianza (6) = %d\n",OMEGA(F(P(x + lambdac*d)))-OMEGA(F(x)))
+            printf ("rhs della disuguaglianza (6) = %d\n",sigma*transpose(DOMEGA(F(x), J(x)))*(P(x + lambdac*d)-x))
+                    
+                    x0=x;
+                    x=P(x + lambdap*d); 
+                    sen_2(k)=1;
+                    FLAG=0;
+                    mm(k)=m;
+                    m=mmax+1;                 
+           
+         
+         lambda(k)=lambdap;
+           FLAG=0;
          end
        err= norm(F(x));
+     
        verr(k+1)=err;
        s=d;
+       if(FLAG~=1)
        XXmio(:,end+1)=x;
-   
+        end
      end
 
      
@@ -126,13 +141,14 @@ for i=1:k
        end
     end
 
-
+verr_paper2=[verr_paper(1:4), verr_paper(4),verr_paper(5:9),verr_paper(9), verr_paper(10:12), verr_paper(13), verr_paper(13:end) ];
 plot(verr, 'k')
 ylim([0,max(verr)+1])
 hold on 
+plot(verr_paper2, '-og')
+
 plot(psen1, verr(psen1),'ob' )
 plot(psen2, verr(psen2), 'sr')
-plot(verr_paper, '-og')
 xlabel('ITERAZIONI')
 ylabel('ERRORE')
 hold off
@@ -140,6 +156,6 @@ hold off
 %
 %end
 %end
-
-disp('Il vettore di nu_k per cui funzione è : ')
-nu_k_vera+0.00001
+%
+%disp('Il vettore di nu_k per cui funzione è : ')
+%nu_k_vera+0.00001
