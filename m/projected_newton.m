311aee37c68c700e5c86ce3528e087d8c5a41ac8
Carlo de Falco <carlo.defalco@polimi.it>
2018-01-04 10:54:09 +0100

    complete algorithm

--- m/projected_newton.m
+++ m/projected_newton.m
@@ -1,5 +1,9 @@
-function [x, err, mm, ee] = projected_newton (J, F, x0, bounds)
+function [x, err, mm, ee, ff, ll] = projected_newton (J, F, x0, bounds)
 
+  maxit    = 2000;
+  errtol   = 1e-12;
+  maxdamp  = 20;
+  
   P        = @(x) min (max (x, bounds (:, 1)), bounds (:, 2));
 
   x        = x0;
@@ -7,51 +11,103 @@ function [x, err, mm, ee] = projected_newton (J, F, x0, bounds)
 
 
   lambda0  = .5;
-  gamma    =  1;
-  eta      = .5;
-  alpha    = 2;
-  t        = 1e-4;
+  lambda0G = .8;
+  gamma    = .8;
+  etamax   = .9;
+  eta      =  7.65518617913987e-01;
+  eta0     =  7.65518617913987e-01;
+  alpha    =  (1 + sqrt (5)) / 2;
+  t        =  1.0e-4;
+  sigma    =  1.0e-1;
+  flag     =  false;
 
-
-  for in = 1 : 200
-
-    res = F(x);
+  resnew = F (x);
+  normresnew = norm (resnew); 
+  
+  for in = 1 : maxit
     
-    err(in) = norm (res);
-    if (err(in) <= 1e-12)
-      break
-    endif
-    jac = J(x);
-    
-    if (in > 1)
-      eta = min (gamma * (err(in) / err(in-1))^alpha, .3);
-      maxeta = gamma*ee(in-1)^alpha;
-      if (maxeta < .3 && maxeta > .1)
-        eta = max (eta, maxeta);
-      endif
-    endif
-    ee(in) = eta;
-    
-    %%d = jac \ ((- res));
-    tol = eta;
-    [d, flag, relres] = gmres (jac, - res, n, tol, n, [], [], zeros (size (x)));
-    
-
-    for m = 0:36
-      lambda = lambda0 ^ m;
-      xnew = P(x+lambda*d);
-      if (norm (F(xnew)) <= ((1 - t*lambda * (1-eta)) * err(in)))
+    res     = resnew;
+    normres = normresnew;
+    err(in) = normres; 
+    jac     = J (x);
+    printf ("%d ", in)
+    if (! flag)
+      ff(in) = flag;
+      if (normres <= errtol)
+        ff(in) = flag;
+        mm(in) = 0;
         break
-      endif 
-    endfor
+      endif
+      
+      if (in > 1)
+        eta = gamma * (err(in) / err(in-1)) ^ alpha;
+        maxeta = gamma * ee(in-1) ^ alpha;
+        if (maxeta > .1)
+          eta = max (eta, maxeta);
+        endif
+      endif
+      %%eta = eta0 * min (1/(in+1)^alpha, normres/(2*err(1)));
+      ee(in) = eta;
+            
+      d = gmres (jac, - res, n, eta, n, [], [], zeros (size (x)));      
 
+      flag = true;
+      for m = 0 : maxdamp
+        lambda = lambda0 ^ m;
+        xnew = P (x + lambda * d);
+        resnew = F (xnew); 
+        normresnew = norm (resnew);
+        printf ("\tPN %g %g %g\n", normresnew,
+                (1 - t * lambda * (1-eta)), normres)
+        if (normresnew <= ((1 - t * lambda * (1-eta)) * normres))
+          flag = false;
+          x = xnew;
+          break
+        endif
+      endfor
+      ll(in) = lambda;
+    endif
+
+    if (flag)
+      ff(in) = flag;
+      d = - jac' * res;
+      flag = true;
+      for m = 0 : maxdamp 
+        lambda = lambda0G ^ m;
+        xnew = P (x + lambda * d);
+        resnew = F (xnew);
+        normresnew = norm (resnew);
+        printf ("\tPG %g %g %g %g\n", 
+                ((1/2) * normresnew^2), ...
+                ((1/2) * normres^2), ...
+                (sigma * (d' * (x - xnew))), ...
+                ((1/2) * normres^2 -
+                 sigma * (d' * (x - xnew))))
+        if ((1/2) * normresnew^2  <=
+            ((1/2) * normres^2 -
+             sigma * (d' * (x - xnew)))
+            || lambda * (norm (d, inf)) <=
+               10 * eps (norm (x, inf)));
+          flag = false;
+          x = xnew;
+          break
+        endif
+      endfor
+            
+      ee(in) = eta;
+      ll(in) = lambda;
+    endif
+
+    plot (err)
+    drawnow
     mm(in) = m;
-    x = xnew;
-    
-    %plot (1:n, x, 1:n, ones (n, 1))
-    %axis ([1, numel(x), 0, 2])
-    %drawnow
 
   endfor
 
+  err = err(:);
+  mm  = mm(:);
+  ee  = ee(:);
+  ff  = ff(:);
+  ll  = ll(:);
+  
 endfunction
--- m/projected_newton_demo.m
+++ m/projected_newton_demo.m
@@ -1,8 +1,8 @@
 clear all
 close all
 
-[J, F, x0, bounds] = benchmark_problems ("diffreactmonotone");
-[x, err, mm, ee] = projected_newton (J, F, x0, bounds);
+[J, F, x0, bounds] = benchmark_problems ("chenetal");
+[x, err, mm, ee, ff, ll] = projected_newton (J, F, x0, bounds);
 
 figure
 plot (x)
